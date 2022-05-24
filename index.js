0ccfdb1ca77071ca1d4a2d7b0fbc3303f014780c
barreirobenjamin <barreirobenjamin@gmail.com>
2022-05-24 17:27:09 -0300

    add cors header

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const router = require('./routes.js')
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 
-app.use(cors(corsOptions));
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
