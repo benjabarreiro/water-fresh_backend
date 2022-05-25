cee41751fe927f76efef8f33b79ce6d3a37f85bb
barreirobenjamin <barreirobenjamin@gmail.com>
2022-05-25 14:55:14 -0300

    change header config

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ const router = require('./routes.js')
 
 
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  
+    next();
+  });
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
